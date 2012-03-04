961c7c898d3f02e78b1d40551b074ac78f31d345
wjb <jaybob413@gmail.com>
2012-03-04 15:08:36 -0500

    modify dalvik.vm settings for GTablet

--- build/tablet-dalvik-heap.mk
+++ build/tablet-dalvik-heap.mk
@@ -17,6 +17,6 @@
 # Provides overrides to configure the Dalvik heap for a standard tablet device.
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=5m \
-    dalvik.vm.heapgrowthlimit=48m \
-    dalvik.vm.heapsize=256m
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapgrowthlimit=32m \
+    dalvik.vm.heapsize=64m
