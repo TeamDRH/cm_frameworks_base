76da493a5c767ebb244d52c49b114c761fc1fa90
wjb <jaybob413@gmail.com>
2012-03-28 13:57:52 -0400

    modify default dalvik heap settings
    Change-Id: Iec186492148c797433c7c842f0a68af3fd1b9546

--- build/tablet-dalvik-heap.mk
+++ build/tablet-dalvik-heap.mk
@@ -18,5 +18,5 @@
 
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapstartsize=8m \
-    dalvik.vm.heapgrowthlimit=32m \
-    dalvik.vm.heapsize=64m
+    dalvik.vm.heapgrowthlimit=64m \
+    dalvik.vm.heapsize=128m
