f94136eeff0a3bc5d055abd8350ce77fbd792630
Alexander Hofbauer <alex@derhofbauer.at>
2012-04-12 01:24:24 +0200

    Load a device specific key handler
    Configurable via overlay settings config_deviceKeyHandlerLib and
    config_deviceKeyHandlerClass.
    
    Change-Id: Iec99d52f4a665e59cd13939af62f9b5b3becd375

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -29,6 +29,7 @@ import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -67,6 +68,8 @@ import com.android.internal.statusbar.IStatusBarService;
 import com.android.internal.telephony.ITelephony;
 import com.android.internal.view.BaseInputHandler;
 import com.android.internal.widget.PointerLocationView;
+
+import dalvik.system.DexClassLoader;
 
 import android.util.DisplayMetrics;
 import android.util.EventLog;
@@ -151,6 +154,9 @@ import java.io.FileDescriptor;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -262,6 +268,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         sApplicationLaunchKeyCategories.append(
                 KeyEvent.KEYCODE_CALCULATOR, Intent.CATEGORY_APP_CALCULATOR);
     }
+
+    Object mDeviceKeyHandler = null;
+    Method mDeviceKeyHandlerMethod = null;
 
     /**
      * Lock protecting internal state.  Must not call out into window
@@ -1125,6 +1134,32 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             screenTurningOn(null);
         } else {
             screenTurnedOff(WindowManagerPolicy.OFF_BECAUSE_OF_USER);
+        }
+
+        String deviceKeyHandlerLib = mContext.getResources().getString(
+                com.android.internal.R.string.config_deviceKeyHandlerLib);
+
+        String deviceKeyHandlerClass = mContext.getResources().getString(
+                com.android.internal.R.string.config_deviceKeyHandlerClass);
+
+        if (!deviceKeyHandlerLib.equals("") && !deviceKeyHandlerClass.equals("")) {
+            DexClassLoader loader =  new DexClassLoader(deviceKeyHandlerLib,
+                    new ContextWrapper(mContext).getCacheDir().getAbsolutePath(),
+                    null,
+                    ClassLoader.getSystemClassLoader());
+            try {
+                Class<?> klass = loader.loadClass(deviceKeyHandlerClass);
+                Constructor<?> constructor = klass.getConstructor(Context.class);
+                mDeviceKeyHandler = constructor.newInstance(mContext);
+                mDeviceKeyHandlerMethod = klass.getDeclaredMethod(
+                        "handleKeyEvent", KeyEvent.class);
+
+                Log.d(TAG, "Loaded device key handler");
+            } catch (Exception e) {
+                Slog.d(TAG, "Could not get device key Handler "
+                        + deviceKeyHandlerClass + " from class "
+                        + deviceKeyHandlerLib, e);
+            }
         }
     }
 
@@ -2022,6 +2057,16 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 && (metaState & mRecentAppsDialogHeldModifiers) == 0) {
             mRecentAppsDialogHeldModifiers = 0;
             showOrHideRecentAppsDialog(RECENT_APPS_BEHAVIOR_DISMISS_AND_SWITCH);
+        }
+
+        if (mDeviceKeyHandler != null && mDeviceKeyHandlerMethod != null) {
+            try {
+                Integer ret = (Integer) mDeviceKeyHandlerMethod.invoke(
+                        mDeviceKeyHandler, event);
+                return ret;
+            } catch (Exception e) {
+                Slog.d(TAG, "Could not invoke device key handler", e);
+            }
         }
 
         // Let the application handle the key.
